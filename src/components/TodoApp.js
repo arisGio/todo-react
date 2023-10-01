ce8406d496cdff3f825b38ecedb0bbee234420a7
Aris Giotis <apjoti@gmail.com>
2023-10-01 16:21:57 +0300

    Add JSX and handlers for adding, removing and completing a todo item

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import TodoForm from "./TodoForm";
+import TodoItem from "./TodoItem";
 
-const TodoApp = () => {
-  return <h1>App</h1>;
-};
+function TodoApp() {
+  const [todos, setTodos] = useState([]);
+
+  /**
+   * Handles new form input
+   * @param {*} todoText
+   */
+  const handleAddTodo = (todoText) => {
+    const newTodo = { id: Date.now(), text: todoText, completed: false };
+    setTodos([...todos, newTodo]);
+  };
+
+  /**
+   * Handles complete action from user
+   * @param {*} todoId
+   */
+  const handleCompleteTodo = (todoId) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === todoId) {
+          return { ...todo, completed: !todo.completed };
+        }
+        return todo;
+      })
+    );
+  };
+
+  /**
+   * Handles removal of todo item
+   * @param {*} todoId
+   */
+  const handleRemoveTodo = (todoId) => {
+    setTodos(todos.filter((todo) => todo.id !== todoId));
+  };
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            onCompleteTodo={handleCompleteTodo}
+            onRemoveTodo={handleRemoveTodo}
+          />
+        ))}
+        <TodoForm onAddTodo={handleAddTodo} />
+      </div>
+    </div>
+  );
+}
 
 export default TodoApp;
